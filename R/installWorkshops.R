82cd2ee2dc6f7ddf1ac8c22a8f5f52327cbdd55b
LiNk-NY <marcel.ramosperez@roswellpark.org>
2019-05-24 11:55:04 -0400

    update code

--- R/installWorkshops.R
+++ R/installWorkshops.R
@@ -1,14 +1,29 @@
-.branch <- function(remotes) {
+.repobranch <- function(remotes) {
     hasBranch <- grepl("tree", remotes, fixed = TRUE)
     branches <- basename(remotes[hasBranch])
     bremotes <- gsub("\\/tree.*", "", remotes)
     bremotes <- file.path(basename(dirname(bremotes)), basename(bremotes))
-    data.frame(repos = bremotes, branches = ifelse(hasBranch, branches, "master"))
+    data.frame(repos = bremotes, refs = ifelse(hasBranch, branches, "master"))
 }
 
-.readIssues <- function(repository) {
-    issues <- fromJSON(
-        paste0("https://api.github.com/repos/", repository, "/issues")
+.getRemotesOnline <- function(repository, location, branch) {
+    baseurl <- switch(location, github = "https://raw.githubusercontent.com")
+    desc <- file.path(baseurl, repository, branch, "DESCRIPTION")
+    tf <- tempfile()
+    download.file(desc, tf)
+    .readRemotes(tf)
+}
+
+.readRemotes <- function(descfile) {
+    dcf <- read.dcf(descfile, "Remotes")
+    trimws(strsplit(dcf, "[, \n]+")[[1]])
+}
+
+.readIssues <- function(repository, location) {
+    location <- switch(location, github = "https://api.github.com/repos/",
+        stop("Non-GitHub locations not yet supported"))
+    issues <- jsonlite::fromJSON(
+        paste0(location, repository, "/issues")
     )
     iswork <- grepl("[Workshop]", issues$title, fixed = TRUE)
     issues <- lapply(issues, function(g) g[iswork])
@@ -16,30 +31,37 @@
     urlidx <- vapply(bodies, function(x) which(grepl("https", x))[[1L]], integer(1L))
     repos <- mapply(function(x, y) x[y], bodies, urlidx)
     repos <- gsub("\\.git", "", repos)
-    .branch(repos)
+    .repobranch(repos)
+}
+
+## git2r::clone repository first and get local location
+.addRemotes <- function(local_repo, reposrefs) {
+    remotes <- apply(reposrefs, 1L, function(line) {
+        if (!identical(line[[2]], "master"))
+            paste(line[[1]], line[[2]], sep = "@")
+        else
+            line[[1]]
+    })
+    desc::desc_add_remotes(remotes, local_repo)
+}
+
+.installIssues <- function(repository, location) {
+    rebranch <- .readIssues(repository, location)
+    apply(rebranch, 1L, function(x) {
+        remotes::install_github(repo = x[[1]], ref = x[[2]])
+    })
 }
 
 #' Install workshops from locations as indicated by main repository
 #'
 #' This function looks at the 'issues' page of the main repository and installs
 #' from the workshop URLs tagged as `[Workshop]`
-#'
+#   '
 #' @param repository A single string indicating the 'username/repository' of the
-#'   GitHub repository
+#'   main book-building GitHub repository
 #' @param location Website location of the main repository (default 'github')
-#' @param branch Indicates the branch from where to pull the DESCRIPTION file
-#'   (default 'master')
 #'
 #' @export
-installWorkshops <- function(repository, location="github", branch="master") {
-    baseurl <- switch(location, github = "https://raw.githubusercontent.com")
-    desc <- file.path(baseurl, repository, branch, "DESCRIPTION")
-    tf <- tempfile()
-    download.file(desc, tf)
-    dcf <- read.dcf(tf, "Remotes")
-    remotes <- trimws(strsplit(dcf, "[, \n]+")[[1]])
-    rebranch <- .readIssues(remotes)
-    apply(rebranch, 1L, function(x) {
-        remotes::install_github(x[[1]], x[[2]])
-    })
+installWorkshops <- function(repository, location="github") {
+    remotes <- .installIssues(repository, location)
 }
